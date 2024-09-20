523b98f586a888b4b7e4c728284bb6a272d765f4
DaniKesede <danieldsousa76@gmail.com>
2024-09-19 22:29:24 -0300

    Utilizando o useEffect

--- pw3-daniel-app-libri/src/COMPONENTS/pages/CreateBooks.jsx
+++ pw3-daniel-app-libri/src/COMPONENTS/pages/CreateBooks.jsx
@@ -1,10 +1,37 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import style from './CreateBooks.module.css';
 import Input from "../forms/Input";
 import Select from "../forms/Select";
 import Button from "../forms/Button";
 
 const CreateBooks = () => {
+    /* RECUPERA DOS DADOS DA APIREST */
+    useEffect( ()=>{
+        fetch('http://localhost:5000/listagemCateorias', {
+            method: 'GET',
+            headers:{
+                'Content-Type':'application/json',
+                'Access-Control-Allow-Origin':'*',
+                'Access-Control-Allow-Headers':'*',
+            }
+        })
+        .then(
+            (resp)=>
+                //console.log('RESPOSTA' + resp)
+                resp.json()
+            
+        ).then(
+            (data)=>{
+                console.log(' DATA: ' + data.data[0].nome_categoria)
+            }
+        ).catch(
+            (error)=>{
+                console.log(error)
+            }
+        )
+    },[]);
+
     return (
         <section className={style.create_book_container}>
             <h1>CADASTRO DE LIVROS</h1>
